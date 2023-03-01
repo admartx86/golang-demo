474a508d9f1e7f6fc0afb753703ad79cf847ad1d
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-03-01 00:33:00 -0600

    Add sprite (just to test drawing to the screen). Add assets folder (filled with several sprites and sounds just for testing).

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github.com/mart3318/golang-demo
 
 go 1.18
 
+require github.com/hajimehoshi/ebiten v1.12.12
+
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
-	github.com/hajimehoshi/ebiten v1.12.12 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20200801110659-972c09e46d76 // indirect
-	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
-	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
+	golang.org/x/exp/shiny v0.0.0-20230224173230-c95f2b4c22f2 // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/mobile v0.0.0-20221110043201-43a038452099 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"image/color"
 	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
 	screenWidth  = 640
 	screenHeight = 480
 )
+
+var (
+	spriteImage *ebiten.Image
+	sprite      = &Sprite{
+		X: screenWidth / 2,
+		Y: screenHeight / 2,
+	}
+)
+
+type Sprite struct {
+	X float64
+	Y float64
+}
 
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
@@ -19,13 +33,31 @@ func update(screen *ebiten.Image) error {
 
 	screen.Fill(color.RGBA{10, 66, 202, 191})
 
-	ebitenutil.DebugPrint(screen, "Hello World!")
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		sprite.X -= 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		sprite.X += 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		sprite.Y += 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		sprite.Y -= 1
+	}
+
+	screen.DrawImage(spriteImage, &ebiten.DrawImageOptions{})
 
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Hello World"); err != nil {
+	var err error
+	spriteImage, _, err = ebitenutil.NewImageFromFile("assets/DinoSprites-doux.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "golang-demo"); err != nil {
 		log.Fatal(err)
 	}
 }
