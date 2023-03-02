9422d82452f41fa6d14266c8d98b456b7517ec69
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-03-01 18:50:23 -0600

    Add meat sprites across the screen.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/color"
 	"log"
 
@@ -9,19 +11,37 @@ import (
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
+	screenWidth  = 1280
+	screenHeight = 720
 )
 
 var (
 	spriteImage *ebiten.Image
 	sprite      = &Sprite{
-		X: screenWidth / 2,
-		Y: screenHeight / 2,
+		X: 373,
+		Y: 73,
+		//X: screenWidth / 2,
+		//Y: screenHeight / 2,
 	}
 )
 
 type Sprite struct {
+	X float64
+	Y float64
+}
+
+var (
+	meatImage *ebiten.Image
+	meats     []*Meat = []*Meat{
+		{X: 415, Y: 300},
+		{X: 500, Y: 500},
+		{X: 700, Y: 100},
+		{X: 950, Y: 500},
+		{X: 820, Y: 200},
+	}
+)
+
+type Meat struct {
 	X float64
 	Y float64
 }
@@ -34,36 +54,69 @@ func update(screen *ebiten.Image) error {
 	screen.Fill(color.RGBA{10, 66, 202, 191})
 
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		sprite.X -= 5
+		sprite.X -= 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		sprite.X += 5
+		sprite.X += 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		sprite.Y += 5
+		sprite.Y += 2
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		sprite.Y -= 5
+		sprite.Y -= 2
 	}
 
-	//screen.DrawImage(spriteImage, &ebiten.DrawImageOptions{})
+	for _, meat := range meats {
+		geo2 := ebiten.GeoM{}
+		geo2.Translate(meat.X, meat.Y)
 
-	screen.DrawImage(spriteImage, &ebiten.DrawImageOptions{
-		GeoM:          ebiten.TranslateGeo(sprite.X, sprite.Y),
+		screen.DrawImage(meatImage, &ebiten.DrawImageOptions{
+			GeoM:          geo2,
+			ColorM:        ebiten.ColorM{},
+			CompositeMode: 0,
+		})
+	}
+
+	geo := ebiten.GeoM{}
+	geo.Translate(-float64(spriteImage.Bounds().Dx())/2, -float64(spriteImage.Bounds().Dy())/2)
+	geo.Translate(sprite.X, sprite.Y)
+
+	scaleFactor := 4.0
+	geo.Scale(scaleFactor, scaleFactor)
+
+	width := float64(spriteImage.Bounds().Size().X)
+	height := float64(spriteImage.Bounds().Size().Y)
+	//width := float64(spriteImage.Bounds().Size().X) * scaleFactor
+	//height := float64(spriteImage.Bounds().Size().Y) * scaleFactor
+	geo.Translate(sprite.X-width/2-float64(spriteImage.Bounds().Dx())/2, sprite.Y-height/2-float64(spriteImage.Bounds().Dy())/2)
+
+	screen.DrawImage(spriteImage.SubImage(image.Rect(0, 0, 23, 23)).(*ebiten.Image), &ebiten.DrawImageOptions{
+		GeoM:          geo,
 		ColorM:        ebiten.ColorM{},
 		CompositeMode: 0,
-		Filter:        0,
-		ImageParts:    nil,
-		//Parts:         []ebiten.ImagePart{},
-		//SourceRect:    &image.Rectangle{},
 	})
 
+	//screen.DrawImage(spriteImage, &ebiten.DrawImageOptions{
+	//	GeoM:          geo,
+	//	ColorM:        ebiten.ColorM{},
+	//	CompositeMode: 0,
+	//})
+
+	positionText := fmt.Sprintf("Sprite position: (%v, %v)", sprite.X, sprite.Y)
+	ebitenutil.DebugPrint(screen, positionText)
+
 	return nil
+
 }
 
 func main() {
+
 	var err error
 	spriteImage, _, err = ebitenutil.NewImageFromFile("assets/DinoSprites-doux.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	meatImage, _, err = ebitenutil.NewImageFromFile("assets/meat3.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
